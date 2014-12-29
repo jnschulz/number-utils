b3b7cf220ae3e2d8c6184773c0ddeb3529ba5675
Joe Schulz <jschulz@riotgames.com>
2014-12-29 17:19:59 -0600

    Implemented a more efficient version of Primes.primeNumbers.

--- src/main/scala/com/jnschulz/numberutils/Primes.scala
+++ src/main/scala/com/jnschulz/numberutils/Primes.scala
@@ -1,6 +1,9 @@
 package com.jnschulz.numberutils
 
 import collection.immutable.SortedMap
+import gnu.trove.set.hash.TLongHashSet
+import gnu.trove.set.TLongSet
+import gnu.trove.procedure.TLongProcedure
 
 object Primes {
  def isPrime(num: Long): Boolean = {
@@ -13,7 +16,7 @@ object Primes {
     }
   }
 
-  def primeNumbers: Stream[Long] = {
+  private[numberutils] def primeNumbersSlower: Stream[Long] = {
     def addIncrementer(incrementerMap: SortedMap[Long, List[Long]], seed: Long, currentIncrement: Long) = {
       val seeds = seed :: incrementerMap.getOrElse(currentIncrement, List())
       incrementerMap.updated(currentIncrement, seeds)
@@ -38,6 +41,66 @@ object Primes {
     }
 
     2L #:: loop(3L, SortedMap(4L -> List(2L)))
+  }
+
+  /** Not an FP method, but pretty fast... */
+  def primeNumbers: Stream[Long] = {
+    val primesEmitted: TLongSet = new TLongHashSet()
+
+    //Logically consistent but worthless initial values that can be operated on by reinitialize()
+    var intervalLength: Int = 1
+    var minCandidate: Long = 1L
+    var maxCandidate: Long = 1L
+    var candidates: Array[Boolean] = Array(false)
+
+    reinitialize()
+
+    def reinitialize() {
+      //Make a new array of candidates twice the size of the previous to hold the next interval of candidates
+      intervalLength *= 2
+      minCandidate = maxCandidate + 1
+      maxCandidate = maxCandidate + intervalLength
+      candidates = Array.fill[Boolean]( intervalLength )(true)
+
+      //Run the candidates through the sieve of previously emitted primes
+      primesEmitted.forEach(new TLongProcedure {
+        override def execute(prime: Long): Boolean = {
+          updateCandidates(prime)
+          true
+        }
+      })
+    }
+
+    def updateCandidates(prime: Long) {
+      //Start at either prime^2 or the first multiple of prime in the candidate interval
+      val initialComposite = (prime*prime) max ( if (minCandidate % prime == 0) minCandidate else minCandidate + prime - (minCandidate % prime) )
+
+      //Mark the multiples of this prime as invalid candidates
+      (initialComposite to maxCandidate by prime).foreach { i: Long =>
+        if (i >= 0 && i <= maxCandidate)
+          candidates( (i - minCandidate).toInt) = false
+      }
+    }
+
+    def loop(candidate: Long): Stream[Long] = {
+      //Reinitialize if we have run out of candidates
+      if (candidate > maxCandidate) {
+        reinitialize()
+      }
+
+      if ( candidates( (candidate - minCandidate).toInt ) ) {
+        //Emit and update the candidates before looping
+        val prime = candidate
+        updateCandidates(prime)
+        primesEmitted.add(prime)
+        prime #:: loop(candidate + 1L)
+      } else {
+        //Simply loop
+        loop(candidate + 1L)
+      }
+    }
+
+    loop(minCandidate)
   }
 
   /** Not an FP method, but pretty fast... */
--- src/test/scala/com/jnschulz/numberutils/PrimesTest.scala
+++ src/test/scala/com/jnschulz/numberutils/PrimesTest.scala
@@ -8,14 +8,20 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class PrimesTest extends FunSpec with ShouldMatchers {
   describe("Primes") {
-    it("should produce primes correctly") {
-      Primes.primeNumbers.take(10).toList should equal {
+    it("should produce primes correctly, if slowly") {
+      Primes.primeNumbersSlower.take(10).toList should equal {
         List(2L, 3L, 5L, 7L, 11L, 13L, 17L, 19L, 23L, 29L)
       }
     }
 
-    it("should produce detect primes correctly") {
-      (1L to 30L).filter(Primes.isPrime(_)).toList should equal {
+    it("should produce primes correctly") {
+      Primes.primeNumbers.take(1000).toList should equal {
+        Primes.primeNumbersSlower.take(1000).toList
+      }
+    }
+
+    it("should detect primes correctly") {
+      (1L to 30L).filter(Primes.isPrime).toList should equal {
         List(2L, 3L, 5L, 7L, 11L, 13L, 17L, 19L, 23L, 29L)
       }
     }
